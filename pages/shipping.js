1967daa7531201ea7824baf35b5badc8525d3113
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-04-07 00:41:20 +0200

    added shipping page

--- pages/shipping.js
+++ pages/shipping.js
@@ -1,15 +1,138 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
 import { useRouter } from 'next/router';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+
 import { useContext, useEffect } from 'react';
 import { Store } from '../utils.js/Store';
+import Cookies from 'js-cookie';
 
 export default function Shipping() {
-  const router = useRouter();
+  const formSchema = Yup.object().shape({
+    fullName: Yup.string().required('Password is required'),
+    address: Yup.string().required('Password is required'),
+    city: Yup.string().required('Password is required'),
+    postalCode: Yup.string().required('Password is required'),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors },
+    reset,
+    setValue,
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
   const { state, dispatch } = useContext(Store);
-  const { userInfo } = state;
+  const router = useRouter();
+  const { redirect } = router.query;
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state;
 
-  if (!userInfo) {
-    router.push('/login?redirect=/shipping');
-  }
+  useEffect(() => {
+    if (!userInfo) {
+      router.push('/login?redirect=/shipping');
+    }
+    setValue('fullName', shippingAddress.fullName);
+    setValue('address', shippingAddress.address);
+    setValue('city', shippingAddress.city);
+    setValue('postalCode', shippingAddress.postalCode);
+  }, []);
 
-  return <div>shipping</div>;
+  const submitHandler = ({ fullName, address, city, postalCode }) => {
+    dispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: { fullName, address, city, postalCode },
+    });
+    Cookies.set(
+      'shippingAddress',
+      JSON.stringify({ fullName, address, city, postalCode })
+    );
+    router.push('/payment');
+  };
+
+  return (
+    <Layout title='Shipping'>
+      <section className='login flex justify-center h-screen'>
+        <div className='formContainer w-full max-w-md lg:mt-10 mt-8 flex flex-col items-center  '>
+          <h1 className='text-5xl font-semibold pb-2'>Dane Dostawy</h1>
+          <h3 className=''>Cieszymy się, że chcesz do nas dołączyć!</h3>
+          <form
+            onSubmit={handleSubmit(submitHandler)}
+            className='w-full flex flex-col justify-start pl-8 pt-5 mb-4'
+          >
+            <div className='mb-4'>
+              <label
+                className='block text-gray-600 text-sm font-bold mb-2 uppercase'
+                for='fullName'
+              >
+                Imię
+              </label>
+              <input
+                className='h-10 w-[90%] border border-solid border-gray-500 drop-shadow-md outline-none text-xl'
+                type='text'
+                placeholder='Imię'
+                {...register('fullName')}
+              />
+            </div>
+            <div className='mb-4'>
+              <label
+                className='block text-gray-600 text-sm font-bold mb-2 uppercase'
+                for='Address'
+              >
+                Ulica
+              </label>
+              <input
+                className='h-10 w-[90%] border border-solid border-gray-500 drop-shadow-md outline-none text-xl'
+                type='text'
+                placeholder='Ulica'
+                {...register('address')}
+              />
+            </div>
+            <div className='mb-4'>
+              <label
+                className='block text-gray-600 text-sm font-bold mb-2 uppercase'
+                for='username'
+              >
+                Miasto
+              </label>
+              <input
+                className='h-10 w-[90%] border border-solid border-gray-500 drop-shadow-md outline-none text-xl'
+                type='text'
+                placeholder='Miasto'
+                {...register('city')}
+              />
+            </div>
+            <div className='mb-4'>
+              <label
+                className='block text-gray-600 text-sm font-bold mb-2 uppercase'
+                for='username'
+              >
+                Kod Pocztowy
+              </label>
+              <input
+                className='h-10 w-[90%] border border-solid border-gray-500 drop-shadow-md outline-none text-xl'
+                type='text'
+                placeholder='Kod Pocztowy'
+                {...register('postalCode')}
+              />
+            </div>
+            <button
+              type='submit'
+              className='h-10 w-[90%] mt-4 bg-slate-400 rounded-sm uppercase font-semibold'
+            >
+              Kontynuuj
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  );
 }
