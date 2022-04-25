846b8e27405c99c671abaac2c24907bb2e52bcfd
kkuczniak <51820616+kkuczniak@users.noreply.github.com>
2022-04-25 21:47:50 +0200

    header fix

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className='min-h-[630px] relative pt-24 px-0 pb-20 mb-6 lg:text-left text-center'>
+    <header className='min-h-[630px] relative pt-24 px-0 pb-20 mb-6 lg:text-left md:text-left text-center'>
       <video
         loop
         autoPlay
@@ -13,16 +13,16 @@ export default function Header() {
       </video>
       <div className='absolute inset-0 bg-[#0000007a] bg-no-repeat'></div>
       <div className='relative max-w-[1264px] lg:m-20'>
-        <div className='flex text-white flex-col lg:w-2/5 m-3'>
+        <div className='flex text-white flex-col lg:w-2/5 md:w-3/4 m-3'>
           <h1 className='text-5xl tracking-wide  leading-tight'>
             Kosmetyki idealne na wiosenne promienie słońca
           </h1>
-          <h3 className='text-xl pt-5 tracking-wide leading-snug '>
+          <h3 className='text-xl pt-5 tracking-wide leading-snug md:w-3/4 '>
             Sprawdź nowości stworzone dla pielęgnacji skóry by powitać wiosenną
             pogodę bez żadnych zmartwień!
           </h3>
           <Link href='/allProducts'>
-            <a className='mx-auto mt-14 p-2 tracking-wider uppercase border rounded w-auto text-center bg-[#ffffff23]'>
+            <a className='mx-auto md:mx-0 lg:mx-0  mt-14 p-2 tracking-wider uppercase border rounded w-auto md:w-48 text-center bg-[#ffffff23]'>
               Nowości
             </a>
           </Link>
